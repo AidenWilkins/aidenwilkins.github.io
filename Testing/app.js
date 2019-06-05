3b31ad6bbc796d9709e9a980bfb0c9905f580002
Aiden Wilkins <aidencw01@gmail.com>
2019-06-05 12:44:19 -0400

    Testiubng

--- Testing/app.js
+++ Testing/app.js
@@ -1,1 +1,8 @@
-document.getElementById()
+document.getElementById("openFile").addEventListener('change', function() {
+	var fr = new FileReader();
+	fr.onload = function() {
+		document.getElementById("content").textContent = this.result;
+	}
+	alert(this.files[0].name);
+	fr.readAsText(this.files[0]);
+})
